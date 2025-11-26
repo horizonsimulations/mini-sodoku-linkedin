f361b43a39b7902593000cbafca5d695ccef47fb
Mark Szutor <112664262+markszutor@users.noreply.github.com>
2025-11-26 14:01:13 +0200

    Add Level 106 configuration for MiniSudoku

---
+++ src/levels/level106.ts
@@ -0,0 +1,20 @@
+import type { MiniSudokuLevel } from "./types";
+
+const level: MiniSudokuLevel = {
+  slug: "level106",
+  title: "Level 106",
+  description: "Describe the inspiration or difficulty (optional).",
+  githubHandle: "__PR_AUTHOR__",
+  publishedDate: "2025-11-25",
+  gridSize: 6,
+  startGrid: [
+  [null, null, null, null, null, null],
+  [null, 1, 2, 3, 4, null],
+  [null, 2, 4, 1, 3, null],
+  [null, 3, 5, 2, 6, null],
+  [null, 5, 3, 6, 1, null],
+  [null, null, null, null, null, null],
+]
+};
+
+export default level;
